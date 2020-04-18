0d57123cc1b1faca2fd0905cd431643e2aa12aa0
rissuuuu <rissuuuu@gmail.com>
2020-04-18 18:31:36 +0530

    Deep learning udemy

--- Udemy course/Deep Learning/Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Udemy course/Deep Learning/Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -1,8 +1,34 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+
+ct=ColumnTransformer([1,])
+
+labelencoder_X_1=LabelEncoder()
+labelencoder_X_2=LabelEncoder()
 
 dataset=pd.read_csv('Churn_Modelling.csv')
 X=dataset.iloc[:,3:13].values
 y=dataset.iloc[:,13].values
+
+ct = ColumnTransformer([("Country",OneHotEncoder(), [1])], remainder = 'passthrough')
+X = ct.fit_transform(X)
+
+# Male/Female
+labelencoder_X = LabelEncoder()
+X[:, 2] = labelencoder_X.fit_transform(X[:, 2])
+
+X=X[:,1:]
+
+
+
+
+
+
+
+
+
 
