b0261c57b7ed48c227ab3d4ce80fc9c4220a03b7
rissuuuu <rissuuuu@gmail.com>
2020-04-20 20:44:31 +0530

    Data Science

--- Udemy course/Deep Learning/Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
+++ Udemy course/Deep Learning/Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
@@ -1,4 +1,3 @@
-
 #import libraries
 import numpy as np
 import matplotlib.pyplot as pl
@@ -8,6 +7,15 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
+
+from keras.backend.tensorflow_backend import set_session
+import tensorflow as tf
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+config.log_device_placement = True  # to log device placement (on which device the operation ran)
+sess = tf.compat.v1.Session(config=config)
+tf.compat.v1.keras.backend.set_session(sess)
+
 
 #import training set
 data_train=pd.read_csv('Google_Stock_Price_train.csv')
@@ -25,6 +33,35 @@ for i in range(60,1258):
 X_train,y_train=np.array(X_train),np.array(y_train)
 
 X_train=np.reshape(X_train,(X_train.shape[0],X_train.shape[1],1))
+
+regressor=Sequential()
+regressor.add(LSTM(units=50,return_sequences=True,input_shape=(X_train.shape[1],1)))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units=50,return_sequences=True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units=50,return_sequences=True))
+regressor.add(Dropout(0.2))
+
+
+regressor.add(LSTM(units=50,))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(units=1))
+regressor.compile(optimizer='adam',loss='mean_squared_error')
+
+regressor.fit(X_train,y_train,epochs=100,batch_size=32)
+
+
+
+
+
+
+
+
+
+
 
 
 
