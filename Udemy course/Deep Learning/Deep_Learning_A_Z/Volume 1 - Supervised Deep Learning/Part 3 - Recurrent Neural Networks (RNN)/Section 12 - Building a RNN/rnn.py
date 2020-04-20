21792cad594d06c08c34cfb8e4f92f56e3a3cd52
rissuuuu <rissuuuu@gmail.com>
2020-04-20 16:47:02 +0530

    Data Science

--- Udemy course/Deep Learning/Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
+++ Udemy course/Deep Learning/Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
@@ -1,3 +1,49 @@
+
+#import libraries
 import numpy as np
 import matplotlib.pyplot as pl
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+#import training set
+data_train=pd.read_csv('Google_Stock_Price_train.csv')
+training_set=data_train.iloc[:,1:2].values
+sc=MinMaxScaler()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
