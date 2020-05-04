365b5250291b8e3e77dc4c715578746ebb10b1f8
rissuuuu <rissuuuu@gmail.com>
2020-05-04 22:49:28 +0530

    DS

--- ML_Model/App.py
+++ ML_Model/App.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template,requests
 import numpy as np
 import pandas as pd
+from sklearn.externals import joblib
 app=Flask(__name__)
 
 @app.route('/')
-
 def home():
     return render_template('home.html')
 
@@ -12,7 +12,13 @@ def home():
 def predict():
     if request.method=='POST':
         try:
-
+            NewYork=float(request.form['NewYork'])
+            California=float(request.form['California'])
+            Florida=float(request.form['Florida'])
+            RnDSpend=float(request.form['RnDSpend'])
+            AdminSpend=float(request.form['AdminSpend'])
+            MarketSpend=float(request.form['MarketSpend'])
+            pred_args=[RnDSpend,AdminSpend,MarketSpend,California,Florida,NewYork]
 
         except valueError:
     return render_template('predict.html')
