0fdd974754b4a1a36f99035a9a509760879caff2
rissuuuu <rissuuuu@gmail.com>
2020-05-04 20:27:07 +0530

    DS

--- ML_Model/App.py
+++ ML_Model/App.py
@@ -1,3 +1,21 @@
-from flask import Flask
-
+from flask import Flask,render_template,requests
+import numpy as np
+import pandas as pd
 app=Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',method=['GET','POST'])
+def predict():
+    if request.method=='POST':
+        try:
+
+
+        except valueError:
+    return render_template('predict.html')
+
+if __name__ =="__main__":
+    app.run()
