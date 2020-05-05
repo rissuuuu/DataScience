38f77090a90aff01413be3c4c04028837c7409a1
rissuuuu <rissuuuu@gmail.com>
2020-05-05 10:07:53 +0530

    Deploy

--- ML_Model/App.py
+++ ML_Model/App.py
@@ -12,6 +12,18 @@ def home():
 def predict():
     if requet.method=='POST':
         try:
+            NewYork=float(request.form['NewYork'])
+            Florida=float(request.form['Florida'])
+            California=float(request.form['California'])
+            RnDSpend=float(request.form['RnDSpend'])
+            AdminSpend=float(request.form['AdminSpend'])
+            MrktngSpend=float(request.form['MrktngSpend'])
+            pred_args=[RnDSpend,AdminSpend,MrktngSpend,California,Florida,NewYork]
+
+            pred_args_arr=np.array(pred_args)
+        except valueError:
+
+    return render_template('predict.html')
 
 if __name__ =="__main__":
     app.run()
