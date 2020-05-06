233711adda34b9b2df633c0fb3031ddb5d01cd10
rissuuuu <rissuuuu@gmail.com>
2020-05-06 18:30:43 +0530

    ML Model

--- Working with realtime Datasets/banglore house price/application/server.py
+++ Working with realtime Datasets/banglore house price/application/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request,render_template
 import util
-
+import joblib
+file=open('banglore_home_prices_model.pickle','rb')
+model=joblib.load(file)
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,14 +10,19 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/predict_home_price', methods=['POST'])
+@app.route('/predict', methods=['GET','POST'])
 def predict_home_price():
-    total_sqft = float(request.form['total_sqft'])
-    location = request.form['location']
-    bhk = int(request.form['bhk'])
-    bath = int(request.form['bath'])
-
-
+    if request.method=='POST':
+        try:
+            Sqft=float(request.form['Sqft'])
+            Bhk=float(request.form['Bhk'])
+            Bathroom=float(request.form['Bathroom'])
+            location=str(request.form.get('comp'))
+            # predicted= float(util.get_estimated_price("cv raman nagar",Sqft,Bhk,Bathroom))
+            predicted= util.get_estimated_price(location,Sqft,Bhk,Bathroom)
+            return render_template('home.html',prediction=predicted)
+        except valueError:
+            return "Check the values"
 
 if __name__ == "__main__":
     app.run()
