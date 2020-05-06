c8d20ffafe58cf002b94208c2386293092cd2332
rissuuuu <rissuuuu@gmail.com>
2020-05-06 14:33:54 +0530

    ML Model

--- Working with realtime Datasets/banglore house price/Website/Server/server.py
+++ Working with realtime Datasets/banglore house price/Website/Server/server.py
@@ -1,16 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request,render_template
 import util
 
 app = Flask(__name__)
 
-@app.route('/get_location_names', methods=['GET'])
-def get_location_names():
-    response = jsonify({
-        'locations': util.get_location_names()
-    })
-    response.headers.add('Access-Control-Allow-Origin', '*')
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-    return response
 
 @app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
@@ -19,14 +15,7 @@ def predict_home_price():
     bhk = int(request.form['bhk'])
     bath = int(request.form['bath'])
 
-    response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
-    })
-    response.headers.add('Access-Control-Allow-Origin', '*')
 
-    return response
 
 if __name__ == "__main__":
-    print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
     app.run()
